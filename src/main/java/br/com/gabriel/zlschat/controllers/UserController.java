1c625343d19781a63a3a30748d02ce4609319ccf
zielisgabriel <josegabrielas0586@email.com>
2025-09-02 19:23:20 -0300

    feat: getFriends() and updateStatus() added

--- src/main/java/br/com/gabriel/zlschat/controllers/UserController.java
+++ src/main/java/br/com/gabriel/zlschat/controllers/UserController.java
@@ -1,21 +1,22 @@
 package br.com.gabriel.zlschat.controllers;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.gabriel.zlschat.dtos.RequestFriendshipDTO;
+import br.com.gabriel.zlschat.config.AuthenticationUserProvider;
+import br.com.gabriel.zlschat.dtos.FriendDTO;
 import br.com.gabriel.zlschat.dtos.UserDTO;
+import br.com.gabriel.zlschat.dtos.UserStatusDTO;
 import br.com.gabriel.zlschat.models.UserEntity;
 import br.com.gabriel.zlschat.services.UserService;
 import jakarta.validation.Valid;
@@ -26,44 +27,57 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
-    private final SimpMessagingTemplate simpMessagingTemplate;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/register")
-    public ResponseEntity<Void> register(@Valid @RequestBody UserDTO userDTO) {
+    public void register(@Valid @RequestBody UserDTO userDTO) {
         this.userService.register(userDTO);
-        return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/find/{username}")
-    public ResponseEntity<List<UserEntity>> findByUsername(@PathVariable String username) {
+    public List<UserEntity> findByUsername(@PathVariable String username) {
         List<UserEntity> users = this.userService.findByUsername(username);
-        return new ResponseEntity<List<UserEntity>>(users, HttpStatus.OK);
+        return users;
     }
 
     @GetMapping("/me")
-    public ResponseEntity<UserEntity> me() {
+    @ResponseStatus(HttpStatus.OK)
+    public UserEntity me() {
         UserEntity user = this.userService.me();
-        return new ResponseEntity<UserEntity>(user, HttpStatus.OK);
+        return user;
     }
 
-    @MessageMapping("/request.friendship")
-    public void requestFriendship(@Payload RequestFriendshipDTO requestFriendshipDTO) {
-        String senderUsername = requestFriendshipDTO.getSenderUsername();
-        String receiverUsername = requestFriendshipDTO.getReceiverUsername();
-        
+    @PostMapping("/friends/request/{receiverUsername}")
+    @ResponseStatus(HttpStatus.OK)
+    public void requestFriendship(@PathVariable String receiverUsername) {
         this.userService.requestFriendship(receiverUsername);
-        this.simpMessagingTemplate.convertAndSendToUser(receiverUsername, "/queue/friendship.request", senderUsername);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/friends/accept/{senderUsername}")
-    public ResponseEntity<Void> acceptFriendship(@PathVariable String senderUsername) {
+    public void acceptFriendship(@PathVariable String senderUsername) {
         this.userService.acceptFriendship(senderUsername);
-        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/friends/reject/{senderUsername}")
-    public ResponseEntity<Void> rejectFriendship(@PathVariable String senderUsername) {
+    public void rejectFriendship(@PathVariable String senderUsername) {
         this.userService.rejectFriendship(senderUsername);
-        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/friends")
+    public List<FriendDTO> getFriends() {
+        AuthenticationUserProvider authenticationUserProvider = new AuthenticationUserProvider();
+        return this.userService.getMyFriends(authenticationUserProvider.getUsernameOfAuthenticatedUser().toString());
+    }
+
+    @MessageMapping("/user.status")
+    public void updateStatus(UserStatusDTO userStatusDTO, Principal principal) {
+        if (!userStatusDTO.getUsername().equals(principal.getName())) {
+            throw new RuntimeException("Você não pode atualizar o status de outro usuário");
+        }
+        this.userService.updateStatus(userStatusDTO);
     }
 }
