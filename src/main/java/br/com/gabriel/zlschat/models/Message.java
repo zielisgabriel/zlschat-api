89a605b0be81948635463f46167ced4243576298
zielisgabriel <josegabrielas0586@email.com>
2025-09-02 19:47:03 -0300

    feat: Typing username added

--- src/main/java/br/com/gabriel/zlschat/models/Message.java
+++ src/main/java/br/com/gabriel/zlschat/models/Message.java
@@ -3,21 +3,35 @@ package br.com.gabriel.zlschat.models;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import org.springframework.data.mongodb.core.mapping.Document;
-
+import br.com.gabriel.zlschat.enums.MessageStatus;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
-@Document(collection = "messages")
 public class Message {
     private UUID id;
+    
+    @NonNull
+    private UUID tempId;
+
+    @NonNull
     private UUID chatRoomId;
+
+    @NonNull
     private String senderUsername;
+
+    @NonNull
+    private String receiverUsername;
+
+    @NonNull
     private String content;
+
+    private MessageStatus status;
     private LocalDateTime sendAt;
 
     public Message() {
         this.id = UUID.randomUUID();
         this.sendAt = LocalDateTime.now();
+        this.status = MessageStatus.SENT;
     }
 }
