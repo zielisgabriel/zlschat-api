08751311c1c4e4fd2a6e9746fb4bd97b3f1d3c0d
zielisgabriel <josegabrielas0586@email.com>
2025-09-02 19:48:25 -0300

    feat: findById and existsByUsersInChatContains added

--- src/main/java/br/com/gabriel/zlschat/repositories/ChatRoomRepository.java
+++ src/main/java/br/com/gabriel/zlschat/repositories/ChatRoomRepository.java
@@ -1,6 +1,7 @@
 package br.com.gabriel.zlschat.repositories;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -8,5 +9,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import br.com.gabriel.zlschat.models.ChatRoom;
 
 public interface ChatRoomRepository extends MongoRepository<ChatRoom, UUID> {
+    boolean existsByUsersInChatContains(String username);
     List<ChatRoom> findByUsersInChatContains(String username);
+    Optional<ChatRoom> findById(UUID id);
 }
