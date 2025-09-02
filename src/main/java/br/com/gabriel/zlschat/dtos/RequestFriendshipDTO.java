49713155658d64a993e30829806d5055412dab7f
zielisgabriel <josegabrielas0586@email.com>
2025-09-02 19:44:52 -0300

    feat: Annotation to verifier attributes

--- src/main/java/br/com/gabriel/zlschat/dtos/RequestFriendshipDTO.java
+++ src/main/java/br/com/gabriel/zlschat/dtos/RequestFriendshipDTO.java
@@ -1,9 +1,13 @@
 package br.com.gabriel.zlschat.dtos;
 
+import org.hibernate.validator.constraints.UUID;
+
 import lombok.Data;
 
 @Data
 public class RequestFriendshipDTO {
+    @UUID
     private String senderUsername;
+    @UUID
     private String receiverUsername;
 }
