8f8cedecd0895ef7c7060de9757abbe1a53db0c3
zielisgabriel <josegabrielas0586@email.com>
2025-09-02 19:46:39 -0300

    .

--- src/main/java/br/com/gabriel/zlschat/models/ChatRoom.java
+++ src/main/java/br/com/gabriel/zlschat/models/ChatRoom.java
@@ -3,16 +3,12 @@ package br.com.gabriel.zlschat.models;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import br.com.gabriel.zlschat.enums.ChatRoomType;
 import lombok.Data;
 
 @Data
-@Document(collection = "chatRooms")
 public class ChatRoom {
-    @Id
     private UUID id;
     private ChatRoomType chatRoomType;
     private List<String> usersInChat;
