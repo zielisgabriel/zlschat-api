c4fe04ad504e3978a7670a984cdbae325ef40105
zielisgabriel <josegabrielas0586@email.com>
2025-09-02 19:49:25 -0300

    feat: findById, existsByTempIdAndChatRoomId and existsByIdAndSenderUsername added

--- src/main/java/br/com/gabriel/zlschat/repositories/MessageRepository.java
+++ src/main/java/br/com/gabriel/zlschat/repositories/MessageRepository.java
@@ -1,12 +1,16 @@
 package br.com.gabriel.zlschat.repositories;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import br.com.gabriel.zlschat.models.Message;
 
-public interface MessageRepository extends MongoRepository<Message, String> {
+public interface MessageRepository extends MongoRepository<Message, UUID> {
     List<Message> findByChatRoomId(UUID chatRoomId);
+    Optional<Message> findById(UUID id);
+    boolean existsByTempIdAndChatRoomId(UUID tempId, UUID chatRoomId);
+    boolean existsByIdAndSenderUsername(UUID id, String senderUsername);
 }
